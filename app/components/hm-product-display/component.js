57b3ba34696525e7e34228d766f76d6f5b9664fa
theron humiston <theron@groupon.com>
2016-03-13 19:08:43 -0500

    add features to product, clean up product index page

--- app/components/hm-product-display/component.js
+++ app/components/hm-product-display/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   selectedTab: 'description',
   showDescription: Ember.computed.equal('selectedTab', 'description'),
-  showSpecs: Ember.computed.equal('selectedTab', 'specs'),
+  showFeatures: Ember.computed.equal('selectedTab', 'features'),
   showManual: Ember.computed.equal('selectedTab', 'manual'),
   showWhereToBuy: Ember.computed.equal('selectedTab', 'whereToBuy')
 });
